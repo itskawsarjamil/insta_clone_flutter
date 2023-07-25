8ffff8a2b3f3df572c6ebb5b24ee5251c9572361
Kawsar Jamil <Kawsarjamil726@gmail.com>
2023-07-25 14:48:59 +0600

    firebase setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout_screen.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
 import 'package:insta_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyB_Ft8e-Xi0FZjkPBotx-XxctnAHObFYsg",
+          appId: "1:405438005664:web:faaf95dc7a086c7561e4c5",
+          messagingSenderId: "405438005664",
+          projectId: "insta-clone-ca388",
+          storageBucket: "insta-clone-ca388.appspot.com",
+        ),
+      );
+    }
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
